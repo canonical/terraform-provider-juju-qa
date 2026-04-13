a9238dac399426e2fb0ef64a1c4681cffe0154ff
Luci Brănescu <luci.branescu@canonical.com>
2026-04-13 15:24:22 +0100

    feat: first running k8s plan

--- tests_machine/canonical_k8s/main.tf
+++ tests_machine/canonical_k8s/main.tf
@@ -23,20 +23,6 @@ resource "juju_model" "this" {
   name = module.model.name
 }
 
-resource "juju_application" "k8s_worker" {
-  name  = "k8s-worker"
-  model_uuid = juju_model.this.uuid
-
-  charm {
-    name     = "k8s-worker"
-    channel  = "1.35/stable"
-    base     = "ubuntu@24.04"
-  }
-
-  constraints       = "arch=${var.arch} tags=${var.tags}"
-  units             = 1
-}
-
 resource "juju_application" "k8s" {
   name  = "k8s"
   model_uuid = juju_model.this.uuid
@@ -47,10 +33,48 @@ resource "juju_application" "k8s" {
     base     = "ubuntu@24.04"
   }
 
+  config = {
+    "dns-enabled" = true
+    "dns-cluster-domain" = "cluster.local"
+  }
+
   expose {
     cidrs = "0.0.0.0/0"
   }
 
   constraints       = "arch=${var.arch} tags=${var.tags}"
   units             = 1
+}
+
+
+resource "juju_application" "k8s-worker" {
+  name  = "k8s-worker"
+  model_uuid = juju_model.this.uuid
+
+  charm {
+    name     = "k8s-worker"
+    channel  = "1.35/stable"
+    base     = "ubuntu@24.04"
+  }
+
+  expose {
+    cidrs = "0.0.0.0/0"
+  }
+
+  constraints       = "arch=${var.arch} tags=${var.tags}"
+  units             = 2
+}
+
+resource "juju_integration" "k8s-worker-cluster" {
+  model_uuid = resource.juju_model.this.uuid
+
+  application {
+    name = juju_application.k8s.name
+    endpoint = "k8s-cluster"
+  }
+
+  application {
+    name     = juju_application.k8s-worker.name
+    endpoint = "cluster"
+  }
 }

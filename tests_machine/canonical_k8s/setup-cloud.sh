053793f91f8bcc1a74001a0481d11b676843c897
Luci Brănescu <luci.branescu@canonical.com>
2026-04-29 15:21:53 +0100

    chore: arbitrary sleep

--- tests_machine/canonical_k8s/setup-cloud.sh
+++ tests_machine/canonical_k8s/setup-cloud.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -ex
 
+sleep 60 # wait for k8s action to work
 juju run k8s/0 get-kubeconfig | yq -r '.kubeconfig' >> ./kubeconfig
 KUBECONFIG=./kubeconfig juju add-k8s tfqa-k8s --cluster-name=k8s --client --controller=tfqa
